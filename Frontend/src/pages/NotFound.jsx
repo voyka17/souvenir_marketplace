beb4f59ee761c53e05257560d35fe1c12292795a
LJ.Agudelo <leidyjohanna64@gmail.com>
2025-06-02 12:43:06 -0500

    Se modifica pagina de not found

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import backgroundImage from '../assets/images/bannerMovil.png'; // Asegúrate que esta ruta sea correcta
+
 
 import Footer from '../components/layout/Footer.jsx';
 import { FaPoo } from 'react-icons/fa';
@@ -11,24 +11,25 @@ const NotFoundPage = () => {
 
 
       <div
-        className="min-h-screen flex flex-col bg-cover bg-center"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
+        className="min-h-screen flex flex-col bg-cover bg-center bg-[var(--createdlightYellow)]"
+
       >
         {/* Contenido principal */}
-        <main className="flex flex-1 items-center justify-center px-4">
-          <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg text-center max-w-md w-full">
+        <main className="flex flex-1 items-center justify-center px-4 ">
+          <div className=" bg-opacity-90 p-8 rounded-lg shadow-lg text-center max-w-md w-full bg-[var(--createdligthYellosW)]" >
             <h1 className="text-6xl font-bold text-green-700 mb-4">404</h1>
             <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-2">
-              Página no encontrada
+
+              Page not found!
             </h2>
             <p className="text-gray-700 mb-6">
-              Lo sentimos, la página que estás buscando no existe.
+              Sorry, the page you're looking for doesn't exist.
             </p>
             <Link
-              to="/product"
+              to="/home"
               className="inline-block px-6 py-2 text-white bg-green-700 rounded hover:bg-green-800 transition"
             >
-              Volver a Productos
+              return to the home page
             </Link>
           </div>
         </main>
