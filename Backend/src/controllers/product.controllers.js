9b77375c80ff1b3ca29e486f17b750060bd8f142
LJ.Agudelo <leidyjohanna64@gmail.com>
2025-06-09 20:57:19 -0500

    Se crea la logica para la tabla de la Bd product

--- Backend/src/controllers/product.controllers.js
+++ Backend/src/controllers/product.controllers.js
@@ -1,4 +1,9 @@
-import { createProduct, getAllProducts } from "../models/ProductModel.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  updateProduct,
+} from "../models/ProductModel.js";
 
 export const productlist = async (req, res) => {
   const product = await getAllProducts();
@@ -11,4 +16,45 @@ export const productAdd = async (req, res) => {
 
   const newProduct = await createProduct(name, description, price, stock, img); // userId cuando se autentique
   res.status(201).json(newProduct);
+};
+
+export const productUpdate = async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, stock, img } = req.body;
+
+  try {
+    const updatedProduct = await updateProduct(
+      id,
+      name,
+      description,
+      price,
+      stock,
+      img
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Error al actualizar el producto", error });
+  }
+};
+
+export const productDelete = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await deleteProduct(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    res.json({ message: "Producto eliminado correctamente", deletedProduct });
+  } catch (error) {
+    res.status(500).json({ message: "Error al eliminar el producto", error });
+    console.log(error);
+  }
 };
--- Backend/src/models/ProductModel.js
+++ Backend/src/models/ProductModel.js
@@ -14,3 +14,29 @@ export const createProduct = async (name, description, price, stock, img) => {
   );
   return result.rows[0];
 };
+
+export const updateProduct = async (
+  id,
+  name,
+  description,
+  price,
+  stock,
+  img
+) => {
+  const result = await pool.query(
+    `UPDATE product 
+     SET name = $1, description = $2, price = $3, stock = $4, img = $5 
+     WHERE id_product = $6 
+     RETURNING *`,
+    [name, description, price, stock, img, id]
+  );
+  return result.rows[0];
+};
+
+export const deleteProduct = async (id) => {
+  const result = await pool.query(
+    "DELETE FROM product WHERE id_product = $1 RETURNING *",
+    [id]
+  );
+  return result.rows[0];
+};
