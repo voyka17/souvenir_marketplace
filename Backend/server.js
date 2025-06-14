42148bcc30c21bc2d48f3314ae3edcc0f7da600a
xpinayads <xpinayads@gmail.com>
2025-06-13 22:27:15 -0400

    Update port to 4001

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import productsRoutes from './src/routes/productsRoutes.js';
 import userRoutes from './src/routes/userRoutes.js'; 
 
-
 dotenv.config();
 const app = express();
 
